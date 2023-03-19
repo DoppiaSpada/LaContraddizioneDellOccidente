90b49705e7c2cf140dd437c452b1f57c46297f52
Tommaso <tommaso.rip03@gmail.com>
2023-03-19 13:06:38 +0100

    1.0.1
    Just a test in js.

--- code_snippets/script.js
+++ code_snippets/script.js
@@ -3,7 +3,7 @@
 //Non funzionante perchè file ancora fuori dal server. Provvedere!
 
 window.onload = function(){
-    fetch('https://github.com/DoppiaSpada/Projects/blob/main/file.txt')
+    fetch('/main/file.txt')
     .then(response => response.json())
     .then(json => console.log(json))
     .catch(err => console.log("Request failed", err));
