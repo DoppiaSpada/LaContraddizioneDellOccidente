8aa8bcf0f3c03e4ac5dd98cd3f0d73687c7badd8
Tommaso <tommaso.rip03@gmail.com>
2023-03-19 16:32:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function upload(){
     fetch('contents/*.txt')
-    .then(response => response.json())
-    .then(json => console.log(json))
+    .then(response => response[0].text())
+    .then(text => console.log(text))
     .catch(err => console.log("Request failed", err));
 }
