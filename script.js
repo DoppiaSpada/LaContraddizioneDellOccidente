7054df9589249ba8b2de5f0c5bd2e5ecf4d27d31
Tommaso <tommaso.rip03@gmail.com>
2023-03-19 18:17:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = function upload(){
-    fetch('contents')
+    fetch('https://github.com/DoppiaSpada/Krino_Hub/contents')
     .then(response => response.json())
     .then(json => console.log(json))
     .catch(err => console.log("Request failed", err));
