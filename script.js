c4e3e3579e3509ed441217f2bfe1236dc318af40
Tommaso <tommaso.rip03@gmail.com>
2023-03-19 15:57:58 +0100

    1.0.5

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-/* Una prova con fetch */
-
-//Non funzionante perchè file ancora fuori dal server. Provvedere!
-
-window.onload = function(){
-    fetch('contents/file.txt')
-    .then(response => response.text())
-    .then(text => console.log(text))
+function upload(){
+    fetch('contents')
+    .then(response => response.json())
+    .then(json => console.log(json))
     .catch(err => console.log("Request failed", err));
 }
