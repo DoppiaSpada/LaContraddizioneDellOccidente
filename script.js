c0e5868449d5ae774b11e5287b34a1c011b17874
Tommaso <tommaso.rip03@gmail.com>
2023-03-19 18:13:12 +0100

    1.1.0

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-function upload(my_dir){
-    my_dir.forEach(function(file){
-        fetch('contents/' + file)
-        .then(response => response.text())
-        .then(text => console.log(text))
-        .catch(err => console.log("Request failed", err));
-    });
+window.onload = function upload(){
+    fetch('contents')
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log("Request failed", err));
 }
