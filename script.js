36e0f76db2f24feb85ada1dd867a01e2d8fd8573
Tommaso Ripanti <tommaso.rip03@gmail.com>
2024-10-17 23:01:06 +0200

    From KrinoHub to La contraddizione Dell'Occidente
    Website where store link to Diego's book

--- script.js
+++
@@ -1,6 +0,0 @@
-function upload(){
-    fetch('https://github.com/DoppiaSpada/Krino_Hub/tree/main/contents', {mode: 'no-cors'})
-    .then(response => response.json())
-    .then(json => console.log(json))
-    .catch(err => console.log("Request failed", err));
-}
